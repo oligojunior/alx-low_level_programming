94015ca93b8120f43085733cd00cd0af861ed2f5
oligojunior <rkudofia@gmail.com>
2022-10-18 02:35:23 +0000

    Advanced tasks done

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,13 @@
 
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	char str1[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+
+	write(2, str1, 59);
 	return (1);
 }
